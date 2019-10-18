8fa7bcb1d9e3b8ea11abd98dd03afbcabf6c980e
Sanatan Subba <visored24@gmail.com>
2019-10-18 13:15:17 +0545

    blog page changes

--- template-parts/post/preview-list.php
+++ template-parts/post/preview-list.php
@@ -10,7 +10,13 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class( 'flex-md-row box-shadow h-md-250' ); ?>>
-
+<?php
+				// https://developer.wordpress.org/reference/functions/the_post_thumbnail/.
+				the_post_thumbnail(
+					array( 390, 260 ),
+					array( 'class' => 'align-self-center card-img-right flex-auto d-lg-block' )
+				);
+				?>
 		<div class="card-body d-flex flex-column align-items-start">
 			<?php
 			the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
@@ -25,16 +31,11 @@
 			<?php
 		}
 		?>
+		
 			<div class="entry-summary card-text mb-auto">
 				<?php newsmandu_entry_summary(); ?>
 			</div>
 		</div>
-			<?php
-				// https://developer.wordpress.org/reference/functions/the_post_thumbnail/.
-				the_post_thumbnail(
-					array( 390, 260 ),
-					array( 'class' => 'align-self-center card-img-right flex-auto d-lg-block' )
-				);
-				?>
+			
 
 </article><!-- #post-<?php the_ID(); ?> -->
