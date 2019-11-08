ce5758b0cbf1e5ac7e2f05ec097bf2d4cac796ef
Sanatan Subba <visored24@gmail.com>
2019-11-08 15:10:23 +0545

    removal of updated date

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -37,18 +37,14 @@ if ( ! function_exists( 'newsmandu_posted_on' ) ) :
 	 * Prints HTML with meta information for the current post-date/time.
 	 */
 	function newsmandu_posted_on() {
-		$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
-		if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
-			$time_string = '<time class="updated" datetime="%3$s">%4$s</time>';
-		} else {
+		$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time>';
+		if ( get_the_time( 'U' ) ) {
 			$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time>';
 		}
 		$time_string = sprintf(
 			$time_string,
 			esc_attr( get_the_date( DATE_W3C ) ),
-			esc_html( get_the_date() ),
-			esc_attr( get_the_modified_date( DATE_W3C ) ),
-			esc_html( get_the_modified_date() )
+			esc_html( get_the_date() )
 		);
 
 		$posted_on = sprintf(
