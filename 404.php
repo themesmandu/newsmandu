fc3a494516f4bac493283afc4838fde6b1d006cf
Sanatan Subba <visored24@gmail.com>
2019-11-11 12:17:05 +0545

    search page fixes

--- 404.php
+++ 404.php
@@ -11,13 +11,10 @@ get_header();
 ?>
 
 <div class="container">
-
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 		</main>
 	</div><!-- #primary -->
-
 </div><!-- /.container -->
-
 <?php
 get_footer();
--- search.php
+++ search.php
@@ -11,43 +11,46 @@ get_header();
 ?>
 
 <div class="container">
+	<div class="row">
+		<div id="primary" class="content-area <?php newsmandu_content_class(); ?>">
+			<main id="main" class="site-main" role="main">
+			<?php
+			if ( have_posts() ) :
+				?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+				<ul class="search-results list-group list-group-flush pb-4">
 
+				<?php
+					/* Start the Loop */
+				while ( have_posts() ) :
+					the_post();
+
+					/**
+					 * Run the loop for the search to output the results.
+					 * If you want to overload this in a child theme then include a file
+					 * called search-result.php in the template-parts folder.
+					 */
+					get_template_part( 'template-parts/search', 'result' );
+
+					endwhile;
+				?>
+
+				</ul>
+
+				<?php
+				the_posts_navigation();
+				else :
+					get_template_part( 'template-parts/content', 'none' );
+			endif;
+				?>
+
+			</main>
+		</div><!-- #primary -->
 		<?php
-		if ( have_posts() ) :
-			?>
-
-			<ul class="search-results list-group list-group-flush pb-4">
-
-			<?php
-				/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
-
-				/**
-				 * Run the loop for the search to output the results.
-				 * If you want to overload this in a child theme then include a file
-				 * called search-result.php in the template-parts folder.
-				 */
-				get_template_part( 'template-parts/search', 'result' );
-
-				endwhile;
-			?>
-
-			</ul>
-
-			<?php
-			the_posts_navigation();
-			else :
-				get_template_part( 'template-parts/content', 'none' );
-		endif;
-			?>
-
-		</main>
-	</div><!-- #primary -->
-
+		/* Get Sidebar #secondary */
+			get_sidebar();
+		?>
+	</div>
 </div><!-- /.container -->
 
 <?php
