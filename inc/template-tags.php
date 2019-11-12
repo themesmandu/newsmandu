6d9888deed99680b6db4b83a6a9bb96ebf2cfea8
Sanatan Subba <visored24@gmail.com>
2019-11-12 16:15:45 +0545

    adding .bypostauthor  class

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -60,7 +60,7 @@ if ( ! function_exists( 'newsmandu_posted_by' ) ) :
 		$byline = sprintf(
 			/* translators: %s: post author. */
 			esc_html_x( 'By %s', 'post author', 'newsmandu' ),
-			'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
+			'<span class="author vcard"><a class="url fn n bypostauthor" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
 		);
 
 		echo '<span class="byline"> ' . $byline . '</span>'; // WPCS: XSS OK.
